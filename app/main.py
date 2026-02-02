30b35b765371816687c22994abe51167cb998599
Kambam Mohankalyan <97141275+Kambammohankalyan@users.noreply.github.com>
2026-02-02 21:24:41 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -88,6 +88,5 @@ def predict():
         # Handle any other errors gracefully
         return jsonify({'error': f'An error occurred: {str(e)}'}), 500
 
-# This allows you to run the app locally for testing
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
